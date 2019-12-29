c57ca46c94ca756924f84526463652cc5a482ad1
Karolina Michalak <karolina0michalak@gmail.com>
2019-12-29 14:38:39 +0100

    create Form.module.css

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import styles from '../modules/Form.module.css';
+
 
 const Form = props => {
     return (
         <form onSubmit={props.submit}>
-            <input type="text" placeholder="type city" value={props.value} onChange={props.change}/>
+            <input type="text" placeholder="type city here" value={props.value} onChange={props.change}/>
             <button>search</button>
         </form>
     )
